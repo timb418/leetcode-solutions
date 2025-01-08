5888636d0d0925e279ea99cd81422a9535907ad2
Timur <10380147+timb418@users.noreply.github.com>
2025-01-08 23:56:40 +0300

    Create 49_group_anagrams.go

---
+++ solutions/49_group_anagrams.go
@@ -0,0 +1,46 @@
+package solutions
+
+import (
+	"sort"
+	"strings"
+)
+
+func GroupAnagrams(strs []string) [][]string {
+	groups := make(map[string][]string)
+	for _, v := range strs {
+		key := getSortedWordLetters(v)
+		groups[key] = append(groups[key], v)
+	}
+
+	res := make([][]string, 0, len(groups))
+	for _, v := range groups {
+		res = append(res, v)
+	}
+	return res
+}
+
+func getSortedWordLetters(word string) string {
+	letters := strings.Split(word, "")
+	sort.Strings(letters)
+	return strings.Join(letters, "")
+}
+
+func GroupAnagrams2(strs []string) [][]string {
+	m := make(map[[26]int][]string)
+
+	for _, word := range strs {
+		count := [26]int{}
+		for i, _ := range word {
+			count[word[i]-'a']++
+		}
+		m[count] = append(m[count], word)
+	}
+
+	res := make([][]string, 0, len(m))
+	for _, v := range m {
+		res = append(res, v)
+	}
+	return res
+}
+
+
